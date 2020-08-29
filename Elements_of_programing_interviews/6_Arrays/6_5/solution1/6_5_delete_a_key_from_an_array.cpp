c931e1e45224eeab85e72c1e3d33cf98d02c5a9b
Naveen Mittal <naveenmittal04@gmail.com>
2020-08-30 00:49:51 +0530

    fix 6_5 and solution for 6_6

--- Elements_of_programing_interviews/6_Arrays/6_5/solution1/6_5_delete_a_key_from_an_array.cpp
+++ Elements_of_programing_interviews/6_Arrays/6_5/solution1/6_5_delete_a_key_from_an_array.cpp
@@ -10,14 +10,20 @@ using namespace std;
 
 class delete_a_key_from_an_array {
  public:
+  void print_array(int array[], size_t size) {
+    for (int i = 0; i < size; ++i) {
+      cout << array[i] << " ";
+    }
+    cout << endl;
+  }
   void delete_key(int array[], size_t size, int key) {
-    for (int i = 0, j = 0; j < size; ++i, ++j) {
-      if (array[j] == key) {
-        j++;
-      } else {
+    for (int i = 0, j = 0; j < size; ++j) {
+      if (array[j] != key) {
         array[i] = array[j];
-        cout << array[i] << " ";
+        i++;
+        // cout << array[i] << " ";
       }
+      // print_array(array, size);
     }
   }
 };
@@ -26,5 +32,6 @@ int main(int argc, char const *argv[]) {
   delete_a_key_from_an_array object;
   int array[] = {5, 3, 7, 11, 3, 3, 3, 5, 7};
   object.delete_key(array, 9, 3);
+  object.print_array(array, 5);
   return 0;
 }
---
+++ Elements_of_programing_interviews/6_Arrays/6_6/6_6_delete_duplicates_from_sorted_array.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: Naveen Kumar Mittal
+ * Question: 6.5 from EOPI
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class delete_duplicates_from_sorted_array {
+ public:
+  void print_array(int array[], size_t size) {
+    for (int i = 0; i < size; ++i) {
+      cout << array[i] << " ";
+    }
+    cout << endl;
+  }
+  void delete_key(int array[], size_t size) {
+    int i = 0, j = 0;
+    for (; j + 1 < size; ++j) {
+      if (array[j] != array[j + 1]) {
+        array[i] = array[j];
+        i++;
+      }
+      print_array(array, size);
+    }
+    array[i] = array[j];
+  }
+};
+
+int main(int argc, char const *argv[]) {
+  delete_duplicates_from_sorted_array object;
+  int array[] = {2, 3, 5, 5, 7, 11, 11, 11, 13};
+  object.delete_key(array, 9);
+  object.print_array(array, 9);
+  return 0;
+}
